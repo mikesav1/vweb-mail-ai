d58949c9b4c50c0dc85b53caa6289cccbc14eccf
Kim Vase <kimesav@gmail.com>
2026-04-17 13:11:51 +0200

    Refactor email checking logic and loop execution

--- app.py
+++ app.py
@@ -1,27 +1,33 @@
 import imaplib
 import email
 import os
+import time
 
 IMAP_SERVER = "imap.one.com"
-EMAIL = os.getenv("MAIL_USER")
-PASSWORD = os.getenv("MAIL_PASS")
+EMAIL_USER = os.getenv("MAIL_USER")
+EMAIL_PASS = os.getenv("MAIL_PASS")
 
 def check_mail():
+    print("Tjekker mail...")
+
     mail = imaplib.IMAP4_SSL(IMAP_SERVER)
-    mail.login(EMAIL, PASSWORD)
+    mail.login(EMAIL_USER, EMAIL_PASS)
     mail.select("inbox")
 
-    status, messages = mail.search(None, "ALL")
+    status, messages = mail.search(None, "UNSEEN")
     mail_ids = messages[0].split()
 
-    for mail_id in mail_ids[-5:]:
+    print("Nye mails:", len(mail_ids))
+
+    for mail_id in mail_ids:
         status, msg_data = mail.fetch(mail_id, "(RFC822)")
         for response_part in msg_data:
             if isinstance(response_part, tuple):
                 msg = email.message_from_bytes(response_part[1])
-                print("Mail:", msg["subject"])
+                print("Mail:", msg.get("subject"))
 
     mail.logout()
 
-print("Starter mail check...")
-check_mail()
+while True:
+    check_mail()
+    time.sleep(60)
